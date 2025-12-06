6a08c69d8273717edc5a5ae1167530f7fdcf1c1c
Meliyanie <167956841+Meliyanie@users.noreply.github.com>
2025-12-06 13:58:37 +0700

    Update script.js
    melmel gf

--- script.js
+++ script.js
@@ -225,11 +225,9 @@ function startTypewriter() {
     
     const fullMessage = `Hi,
 
-Happy Birthday!
+Happy Birthday Vikki Rahman H.abangnya gemoyy!
 
-Hari ini aku pengen kamu ngerasain semua hal positif dan keajaiban yang cuma bisa didapetin kalo kamu ada di dunia ini. Semoga segala keinginanmu tercapai, apalagi yang kocak-kocak dan gak biasa, karena kamu tuh unik banget! Aku selalu percaya kalau kamu bisa melewati semua tantangan dengan kekuatan dan semangat yang luar biasa.
-
-Terima kasih udah jadi bagian hidup aku yang paling berharga. Kamu bener-bener bikin hari-hari aku jadi lebih berarti dan penuh warna. Semoga di tahun yang baru ini, kamu makin bahagia, makin sukses, dan tentunya makin cantik (walaupun udah cantik banget sih!).
+selamat ulang tahun cayangkuu, semoga panjang umur sehat selalu diperlancarkan rezeki dan dipermudahkan segala urusan nya, semoga segala keinginan abang cepat tercapai, dan sukses selalu, Aamiin ya Allah. makasih cayang yaa udah mau pilih gemoy buat jadi pasangan abang, gemoy senang banget rasanya ada abang hadir di hidup gemoy dari awal kehadiran abang, makasih juga abang selalu menemani gemoy disaat senang, sedih, lagi sakit dan marah marah nya, eeehehe... gemoy bangga punya abang dan gemoy yakin abang pasti bisa melewati semua tantangan dan rintangan yang akan kedepan nya, semangatt truss cayang ku yaa
 
 I love you so much! 💕`;
     
